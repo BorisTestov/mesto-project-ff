1f53a4403797718f499dc901307c039dfe662b81
BorisTestov <boris@the-boris.com>
2024-11-26 01:31:27 +0200

    First iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardData, deleteCallback) {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const image = cardElement.querySelector('.card__image');
 
-// @todo: Функция создания карточки
+    image.alt = cardData.name;
+    image.src = cardData.link;
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector(".card__title").textContent = cardData.name;
+    cardElement.querySelector(".card__delete-button").addEventListener('click', deleteCallback);
 
-// @todo: Вывести карточки на страницу
+    return cardElement;
+}
+
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+}
+
+initialCards.forEach(card => {
+    const cardElement = createCard(card, deleteCard);
+    placesList.append(cardElement);
+})
